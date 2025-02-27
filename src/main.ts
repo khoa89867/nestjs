b4e8c06259959c8384fe6a05f0d4c0a93b2a8657
Tanjiro <a1989@A1989s-MacBook-Pro.local>
2025-02-27 22:02:19 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.useGlobalPipes(new ValidationPipe());
-  // const { httpAdapter } = app.get(HttpAdapterHost);
-  // app.useGlobalFilters(new ExceptionLoggerFilter(httpAdapter));
   await app.listen(3000);
-
 
   
 }
 bootstrap();
+
+export default bootstrap;
