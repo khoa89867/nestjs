cac8139bf2c7036a692d679a1c9c59aed0f4989c
Tanjiro <a1989@A1989s-MacBook-Pro.local>
2025-02-28 21:40:35 +0700

    update

--- dist/upload/service/upload.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { PostRepository } from "src/post/repositories/post.repository";
-export declare class UploadService {
-    private readonly postRepository;
-    constructor(postRepository: PostRepository);
-    uploadFile(file: any): Promise<any>;
-}
--- dist/user/dto/user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateUserDto {
-    email: string;
-    name: string;
-    password: string;
-}
-export declare class LoginUserDto {
-    email: string;
-    password: string;
-}
